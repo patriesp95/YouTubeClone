8fc78b40d537a68a8f00254cde99b59fe85965d0
Patricia Martínez Espert <patrimespert@outlook.com>
2023-10-21 18:51:40 +0200

    clase 2

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Patricia Martínez Espert on 21/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
